09e7522aeebbc043cfcaf2e18fe42d8456b26420
권영각(학원) <younggak@gmail.com>
2019-08-02 14:59:35 +0900

    로긴, 조인 핸들러 추가

---
+++ src/main/java/kr/yi/hairshop/handler/member/LoginHandler.java
@@ -0,0 +1,27 @@
+package kr.yi.hairshop.handler.member;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.ibatis.session.SqlSession;
+
+import kr.yi.hairshop.controller.CommandHandler;
+
+public class LoginHandler implements CommandHandler {
+
+	@Override
+	public String process(HttpServletRequest req, HttpServletResponse res) throws Exception {
+		if(req.getMethod().equalsIgnoreCase("get")) {
+			return "/WEB-INF/view/member/loginForm.jsp";					
+		}else if(req.getMethod().equalsIgnoreCase("post")) {
+			
+			SqlSession sqlSession = null;
+			
+			
+			
+			
+		}
+		return null;
+	}
+
+}
