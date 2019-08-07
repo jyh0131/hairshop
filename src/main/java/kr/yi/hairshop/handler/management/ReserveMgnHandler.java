7b8d036cef852e75af425c9d4f80c80d3c1086ae
김남호 <kimnamo0113@gmail.com>
2019-08-07 14:19:03 +0900

    work수정작업중

--- src/main/java/kr/yi/hairshop/handler/management/ReserveMgnHandler.java
+++ src/main/java/kr/yi/hairshop/handler/management/ReserveMgnHandler.java
@@ -12,9 +12,18 @@ import javax.servlet.http.HttpServletResponse;
 import kr.yi.hairshop.controller.CommandHandler;
 import kr.yi.hairshop.dao.DesignerMapper;
 import kr.yi.hairshop.dao.DesignerMapperImpl;
+import kr.yi.hairshop.dao.EventMapper;
+import kr.yi.hairshop.dao.EventMapperImpl;
+import kr.yi.hairshop.dao.LevelMapper;
+import kr.yi.hairshop.dao.LevelMapperImpl;
+import kr.yi.hairshop.dao.ProductMapper;
+import kr.yi.hairshop.dao.ProductMapperImpl;
 import kr.yi.hairshop.dao.WorkDialogMapper;
 import kr.yi.hairshop.dao.WorkDialogMapperImpl;
 import kr.yi.hairshop.dto.Designer;
+import kr.yi.hairshop.dto.Event;
+import kr.yi.hairshop.dto.Level;
+import kr.yi.hairshop.dto.Product;
 import kr.yi.hairshop.dto.WorkDialog;
 
 public class ReserveMgnHandler implements CommandHandler {
@@ -29,7 +38,18 @@ public class ReserveMgnHandler implements CommandHandler {
 		req.setAttribute("dList", dList);
 		int dNo = dDao.selectMinNo();
 
-		WorkDialogMapper wDao = new WorkDialogMapperImpl();
+		LevelMapper lDao = new LevelMapperImpl();
+		List<Level> lList=lDao.selectLevelByAll();
+		req.setAttribute("lList", lList);
+		
+		ProductMapper pDao=new ProductMapperImpl();
+		List<Product> pList=pDao.selectProductByAll();
+		req.setAttribute("pList", pList);
+		
+		EventMapper eDao = new EventMapperImpl();
+		List<Event> eList=eDao.selectEventByAll();
+		req.setAttribute("eList", eList);
+		
 		Date date = new Date();
 		SimpleDateFormat sf = new SimpleDateFormat("yyyy-MM-dd");
 		Map<String, String> map = new HashMap<String, String>();
